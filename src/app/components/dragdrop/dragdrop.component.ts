3e967678189caff3fe8044c36e4557ec59cb2452
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-03-03 08:00:49 -0500

    changes

--- src/app/components/dragdrop/dragdrop.component.ts
+++ src/app/components/dragdrop/dragdrop.component.ts
@@ -1,11 +1,64 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from "@angular/cdk/drag-drop";
+import { Board } from "src/app/models/board.model";
+import { Column } from "src/app/models/column.model";
 
 @Component({
   selector: "app-dragdrop",
   templateUrl: "./dragdrop.component.html",
   styleUrls: ["./dragdrop.component.scss"],
 })
-export class DragdropComponent {
+export class DragdropComponent implements OnInit {
   @Input() public imageUrl = "assets/bgpoly.png";
   @Input() public title = "Drag & Drop";
+
+  constructor() {}
+  board: Board = new Board("Test Board", [
+    new Column("Ideas", [
+      "Some random idea",
+      "This is another random idea",
+      "build an awesome application",
+    ]),
+    new Column("Research", [
+      "Lorem ipsum",
+      "foo",
+      "This was in the 'Research' column",
+    ]),
+    new Column("Todo", [
+      "Get to work",
+      "Pick up groceries",
+      "Go home",
+      "Fall asleep",
+    ]),
+    new Column("Done", [
+      "Get up",
+      "Brush teeth",
+      "Take a shower",
+      "Check e-mail",
+      "Walk dog",
+    ]),
+  ]);
+
+  ngOnInit() {}
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
